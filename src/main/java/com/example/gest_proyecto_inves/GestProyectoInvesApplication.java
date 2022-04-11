c27d252c35237d1f728f37336f93bf77483fa62c
Pablo Uribe <sistemaspablo21@gmail.com>
2022-04-10 20:15:59 -0500

    Primer commit

---
+++ src/main/java/com/example/gest_proyecto_inves/GestProyectoInvesApplication.java
@@ -0,0 +1,13 @@
+package com.example.gest_proyecto_inves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestProyectoInvesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestProyectoInvesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gest_proyecto_inves/GestProyectoInvesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gest_proyecto_inves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestProyectoInvesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
